03f27ef8fcd620c0a91cdf153ca4fe61983acc10
karthikarun172 <karthikarun172@gmail.com>
2025-07-28 11:38:25 +0530

    feature post added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 })
 
+app.post ('/submit', (req, res) => {
+  res.send('Form submitted successfully!');    
+})
 
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5`000');
